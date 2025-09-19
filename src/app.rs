5c881a211d38b7ba1f06fd7cc16367a02c8d373a
RaineyMisty <tic128@pitt.edu>
2025-09-18 23:28:43 -0400

    fix(app): fix setup camera bug.

--- src/app.rs
+++ src/app.rs
@@ -8,12 +8,15 @@ use bevy::time::Fixed;
 
 pub fn run() {
     App::new()
-        .insert_resource(Time::<Fixed>::from_hz(60))
+        .insert_resource(Time::<Fixed>::from_hz(60.0))
         .add_plugins(DefaultPlugins)
         .add_systems(Startup, setup_camera)
         .run();
 }
 
 fn setup_camera(mut commands: Commands) {
-    commands.spawn(Camera2dBundle::default());
+    commands.spawn((
+        Camera2d,
+        Camera::default(),
+    ));
 }

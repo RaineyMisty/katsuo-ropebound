04eb2840ccaf3addbd2e40b12f62f068ec9dcd26
RaineyMisty <tic128@pitt.edu>
2025-09-26 01:10:36 -0400

    config(physics): find the best spring constant for rope

--- src/config/physics.rs
+++ src/config/physics.rs
@@ -19,4 +19,4 @@ pub const PLAYER_JUMP_FORCE: f32 = 20000.0 * SCALE; // in Newton
 // Rope parameters
 pub const ROPE_REST_LENGTH: f32 = 5.0 * SCALE;  // in pixel
 pub const ROPE_MAX_EXTENSION: f32 = 50.0;  // Maximum extension beyond rest length
-pub const SPRING_CONSTANT: f32 = 500.0;  // in Newton/pixel = kg/s^2
+pub const SPRING_CONSTANT: f32 = 80000.0;  // in Newton/pixel = kg/s^2

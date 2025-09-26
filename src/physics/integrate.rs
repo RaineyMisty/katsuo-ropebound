0bee81721a68ed0fac0fcee7bce2708f2ec1d9ce
RaineyMisty <tic128@pitt.edu>
2025-09-26 00:45:10 -0400

    fix(physics/integrate): fix player stick to the boundary bug

--- src/physics/integrate.rs
+++ src/physics/integrate.rs
@@ -14,7 +14,7 @@ pub fn clean_force_system(
 }
 
 pub fn integrate_force_system(
-    time: Res<Time<Fixed>>,
+    time: Res<Time>,
     mut query: Query<(&mut Momentum, &NetForce)>,
 ) {
     let delta_seconds = time.delta_secs();
@@ -44,26 +44,30 @@ pub fn integrate_velocity_system(
 
 // Force to give a windows boundary
 pub fn boundary(
-    mut query: Query<(&mut Transform, &mut Velocity)>,
+    mut query: Query<(&mut Transform, &mut Velocity, &mut Momentum)>,
 ) {
     let width = 1280.0 - 64.0; // minus player width
     let height = 720.0 - 64.0; // minus player height
-    for (mut transform, mut velocity) in query.iter_mut() {
+    for (mut transform, mut velocity, mut momentum) in query.iter_mut() {
         if transform.translation.x < - width / 2.0 {
             transform.translation.x = - width / 2.0;
             velocity.0.x = 0.0;
+            momentum.0.x = 0.0;
         }
         if transform.translation.x > width / 2.0 {
             transform.translation.x = width / 2.0;
             velocity.0.x = 0.0;
+            momentum.0.x = 0.0;
         }
         if transform.translation.y < - height / 2.0 {
             transform.translation.y = - height / 2.0;
             velocity.0.y = 0.0;
+            momentum.0.y = 0.0;
         }
         if transform.translation.y > height / 2.0 {
             transform.translation.y = height / 2.0;
             velocity.0.y = 0.0;
+            momentum.0.y = 0.0;
         }
     }
 }

ba4a6c26c86de0edecb5d474d8e9f391507ab459
RaineyMisty <tic128@pitt.edu>
2025-09-26 01:04:20 -0400

    fix(physics/gravity): use correct gravity force formular

--- src/physics/gravity.rs
+++ src/physics/gravity.rs
@@ -4,14 +4,15 @@
 // Description: <Gravity system>
 use bevy::prelude::*;
 use crate::config::physics::GRAVITY;
-use crate::components::motion::{NetForce, Gravity};
+use crate::components::motion::{NetForce, Gravity, Mass};
 
 pub fn gravity_system(
-    mut query: Query<(&mut NetForce, &Gravity)>,
+    mut query: Query<(&mut NetForce, &Gravity, &Mass)>,
 ) {
-    for (mut net_force, gravity) in &mut query {
+    for (mut net_force, gravity, mass) in &mut query {
         if gravity.0 {
-            net_force.0 += GRAVITY;
+            // F = m * g
+            net_force.0 += GRAVITY * mass.0;
         }
     }
 }

b7a934645cd0c59c81cbc588d04cc51c61b0c2f1
diana wang <dianawang@dianadeMacBook-Pro.local>
2025-10-07 09:34:00 +0800

    Save the change if rope virualization

--- src/player/player_plugin.rs
+++ src/player/player_plugin.rs
@@ -25,7 +25,7 @@ impl Plugin for PlayerPlugin {
     }
 }
 
-fn spawn_player(mut commands: Commands, asset_server: Res<AssetServer>, spawn_point: Res<PlayerSpawnPoint>, spawn_velocity: Res<PlayerSpawnVelocity>) {
+pub fn spawn_player(mut commands: Commands, asset_server: Res<AssetServer>, spawn_point: Res<PlayerSpawnPoint>, spawn_velocity: Res<PlayerSpawnVelocity>) {
     let transform = Transform::from_translation(spawn_point.position);
     let texture = asset_server.load("spriteguy.png");
     let controls = PlayerControls {
@@ -39,6 +39,8 @@ fn spawn_player(mut commands: Commands, asset_server: Res<AssetServer>, spawn_po
     let mass = Mass(PLAYER_SPAWN_MASS); // make the first player heavier (deleted for now but multiply mass)
     let velocity = Velocity(spawn_velocity.velocity);
     let p1 = commands.spawn(PlayerBundle::new(controls, texture, transform, velocity, mass, jump_controller, ground_state)).insert(FollowedPlayer).id();
+    
+    
     // Spawn a second player for testing
     // This is temporary and will be removed later
     // Ideally we would have a better way
@@ -54,14 +56,14 @@ fn spawn_player(mut commands: Commands, asset_server: Res<AssetServer>, spawn_po
     let jump_controller = JumpController::default();
     let ground_state = GroundState::default();
     let mass = Mass(PLAYER_SPAWN_MASS);
-    let p2 = commands.spawn(PlayerBundle::new(controls, texture, transform, velocity, mass, jump_controller, ground_state)).id();
+    let p2 = commands.spawn(PlayerBundle::new(controls, texture, transform, velocity, mass, jump_controller, ground_state)).insert(FollowedPlayer).id();
 
     // Add p1 and p2 a rope component
     commands.spawn(Rope {
         constraint: RopeConstraint::default(),
         attached_entity_head: p1,
-        attached_entity_tail: p2,
-    });
-    
-    // Ground platform setup
+attached_entity_tail: p2,
+});
+
+// Ground platform setup
 }

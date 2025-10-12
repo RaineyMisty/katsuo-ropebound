4cd5a029c50eae5a9bec51d565ef20c4bf3e6ca9
RaineyMisty <tic128@pitt.edu>
2025-10-12 04:08:34 -0400

    feat(physics): add general physics bundle

---
+++ src/physics/bundle.rs
@@ -0,0 +1,16 @@
+// SPDX-License-Identifier: MIT
+// Copyright (c) 2025 Tingxu Chen
+// Author: Tingxu Chen <tic128@pitt.edu>
+// Description: <General physics bundle>
+use bevy::prelude::*;
+
+use super::component::{Velocity, NetForce, Gravity, Mass, Momentum};
+
+#[derive(Bundle, Clone)]
+pub struct PhysicsBundle {
+    pub velocity: Velocity,
+    pub net_force: NetForce,
+    pub gravity: Gravity,
+    pub mass: Mass,
+    pub momentum: Momentum,
+}

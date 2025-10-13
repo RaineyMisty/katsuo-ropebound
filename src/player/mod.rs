9ef1f01cd2a81841845bd49688830c5ec99f392b
RaineyMisty <tic128@pitt.edu>
2025-10-13 01:28:09 -0400

    feat(player): add a spawn setup

--- src/player/mod.rs
+++ src/player/mod.rs
@@ -20,6 +20,26 @@ pub struct PlayerPlugin;
 impl Plugin for PlayerPlugin {
     fn build(&self, app: &mut App) {
         app.add_event::<PlayerSpawnEvent>()
-        .add_systems(Startup, spawn_player);
+        .add_systems(Startup, queue_for_player_setup_event)
+        .add_systems(Update, spawn_player);
     }
+}
+
+use self::component::ControlScheme;
+
+fn queue_for_player_setup_event(
+    asset_server: Res<AssetServer>,
+    mut events: EventWriter::<PlayerSpawnEvent>,
+) {
+    let tex: Handle<Image> = asset_server.load("portrait_rainey.png");
+    events.write(PlayerSpawnEvent {
+        texture: tex,
+        position: Vec2::new(-500.0,-200.0),
+        controls: ControlScheme{
+            up: KeyCode::KeyW,
+            left: KeyCode::KeyA,
+            right: KeyCode::KeyD,
+        },
+        mass: Some(50.0),
+    });
 }

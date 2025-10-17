85af1d4fda60f58767c294c5a3d6d410748cce76
RaineyMisty <tic128@pitt.edu>
2025-10-17 04:05:14 -0400

    fix(player): fix warning

--- src/player/config.rs
+++ src/player/config.rs
@@ -7,5 +7,5 @@ use bevy::prelude::*;
 const SCALE: f32 = 64.0; // 64 pixels = 1 meter
 
 // Player spawn parameters
-pub(super) const PLAYER_SIZE: Vec2 = Vec2::new(64.0, 64.0);
+pub(super) const PLAYER_SIZE: Vec2 = Vec2::new(SCALE, SCALE);
 pub(super) const PLAYER_SPAWN_MASS: f32 = 120.0; // in kg
--- src/player/spawn.rs
+++ src/player/spawn.rs
@@ -5,6 +5,7 @@
 use bevy::prelude::*;
 
 use super::component::Player;
+use super::config::PLAYER_SPAWN_MASS;
 use super::bundle::PlayerBundle;
 
 use crate::physics::bundle::PhysicsBundle;
@@ -15,11 +16,12 @@ pub(super) fn spawn_player(
     mut events: EventReader<PlayerSpawnEvent>,
     mut req_ctl: EventWriter<RequestControl>
 ) {
+    let spawn_mass = PLAYER_SPAWN_MASS;
     for event in events.read() {
         let transform = Transform::from_translation(event.position.extend(0.0));
         let entity = commands.spawn((
             PlayerBundle::new(event.texture.clone(), transform),
-            PhysicsBundle::new(event.mass.unwrap_or(1.0), true),
+            PhysicsBundle::new(event.mass.unwrap_or(spawn_mass), true),
             Player,
         ))
         .id();

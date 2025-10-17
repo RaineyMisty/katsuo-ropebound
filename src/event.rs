ed57da768901c2f4a44874cc01e985e95d27e59d
RaineyMisty <tic128@pitt.edu>
2025-10-17 03:18:48 -0400

    feat(event): add new global events for player_lifetime, player and control

--- src/event.rs
+++ src/event.rs
@@ -4,18 +4,32 @@
 // Description: <Event>
 use bevy::prelude::*;
 
+/** Physics event
+  *
+  * rope -> physics
+  */
 #[derive(Event, Debug)]
 pub struct ForceEvent {
     pub target: Entity,
     pub force: Vec2,
 }
 
+
+/* Impulse event
+  *
+  * physics -> physics
+  * collision -> physics
+  */
 #[derive(Event, Debug)]
 pub struct ImpulseEvent {
     pub target: Entity,
     pub impulse: Vec2,
 }
 
+/** Player Intent event
+  *
+  * control -> physics
+  */
 #[derive(Event, Debug)]
 pub struct PlayerIntentEvent {
     pub player: Entity,
@@ -28,4 +42,37 @@ pub enum PlayerIntentKind {
     JumpStart,
     JumpHold{ dt: f32 },
     JumpEnd,
+}
+
+/* Player Spawn event
+ *
+ * playerlife -> player
+ */
+#[derive(Event, Debug)]
+pub struct PlayerSpawnEvent {
+    // pub name: String,
+    pub texture: Handle<Image>,
+    pub position: Vec2,
+    pub controls: ControlSpec,
+    pub mass: Option<f32>,
+}
+
+/* Player control spec
+ *
+ * player -> control
+ */
+#[derive(Event, Debug)]
+pub struct RequestControl {
+    pub entity,
+    pub spec: ControlSpec
+}
+
+#[derive(Debug, Clone)]
+pub enum ControlSpec {
+    Keyboard {
+        up: KeyCode,
+        left: KeyCode,
+        right: KeyCode,
+    },
+    Aibot,
 }

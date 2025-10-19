6c7005cb000ac728e77f0d515aee553c917de256
RaineyMisty <tic128@pitt.edu>
2025-10-18 23:57:04 -0400

    feat(lifetime): spawn 3 players

--- src/player_lifetime/player_spawn.rs
+++ src/player_lifetime/player_spawn.rs
@@ -13,11 +13,35 @@ pub(super) fn queue_for_player_setup_event(
     let tex: Handle<Image> = asset_server.load("portrait_rainey.png");
     events.write(PlayerSpawnEvent {
         texture: tex,
-        position: Vec2::new(-500.0,-200.0),
+        position: Vec2::new(-100.0,-200.0),
         controls: ControlSpec::Keyboard {
             up: KeyCode::KeyW,
             left: KeyCode::KeyA,
             right: KeyCode::KeyD,
+        },
+        mass: Some(50.0),
+    });
+
+    let tex: Handle<Image> = asset_server.load("portrait_shawn.png");
+    events.write(PlayerSpawnEvent {
+        texture: tex,
+        position: Vec2::new(-300.0,-200.0),
+        controls: ControlSpec::Keyboard {
+            up: KeyCode::ArrowUp,
+            left: KeyCode::ArrowLeft,
+            right: KeyCode::ArrowRight,
+        },
+        mass: Some(50.0),
+    });
+
+    let tex: Handle<Image> = asset_server.load("portrait_jagger.png");
+    events.write(PlayerSpawnEvent {
+        texture: tex,
+        position: Vec2::new(-500.0,-200.0),
+        controls: ControlSpec::Keyboard {
+            up: KeyCode::KeyI,
+            left: KeyCode::KeyJ,
+            right: KeyCode::KeyL,
         },
         mass: Some(50.0),
     });

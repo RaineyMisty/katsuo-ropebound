276c7308a6f8a5bfb78af7d64b4781843e2bf134
RaineyMisty <tic128@pitt.edu>
2025-10-23 19:21:20 -0400

    feat(physics): add rope dealing file

---
+++ src/physics/rope.rs
@@ -0,0 +1,13 @@
+// SPDX-License-Identifier: MIT
+// Copyright (c) 2025 Tingxu Chen
+// Author: Tingxu Chen <tic128@pitt.edu>
+// Description: <Rope physics>
+use bevy::prelude::*;
+
+use event::RegisterRope;
+
+pub(super) fn rope_register_system(
+    mut commands: Commands,
+    mut events: EventReader<RegisterRope>,
+) {
+}

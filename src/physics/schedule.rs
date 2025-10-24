ddbdf04adcb44fae70201c22c4873fe28cf3c6d1
RaineyMisty <tic128@pitt.edu>
2025-10-24 02:48:01 -0400

    feat(physics): add system set

---
+++ src/physics/schedule.rs
@@ -0,0 +1,12 @@
+// SPDX-License-Identifier: MIT
+// Copyright (c) 2025 Tingxu Chen
+// Author: Tingxu Chen <tic128@pitt.edu>
+// Description: <Physics SystemSet>
+use bevy::prelude::*;
+
+#[derive(SystemSet, Debug, Clone, Eq, PartialEq, Hash)]
+pub(in crate::physics) enum PhysicsSet {
+    Clear,
+    Emit,
+    Integrate,
+}

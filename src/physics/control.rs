92a9d857a2cce5ad2cf3c4d2de520b31bda04043
RaineyMisty <tic128@pitt.edu>
2025-10-24 02:54:55 -0400

    debug(physics): delete debug info

--- src/physics/control.rs
+++ src/physics/control.rs
@@ -18,21 +18,16 @@ pub(super) fn player_intent_to_force(
     let jump_impulse = PLAYER_JUMP_IMPULSE;
 
     for event in intent_events.read() {
-        info!("Processing intent for player {:?}", event.player);
         let (velocity, mut net_force, mut impulse) = match query.get_mut(event.player) {
             Ok(data) => data,
             Err(_) => continue,
         };
-        info!("Current velocity: {:?}", velocity.0);
-        info!("Current net force: {:?}", net_force.0);
-        info!("Current impulse: {:?}", impulse.0);
 
         let mut force_limit = Vec2::ZERO;
         let resistance_constant = move_force / speed_limit;
         let mut jump_impulse = Vec2::ZERO;
         match event.intent {
             PlayerIntentKind::Move { axis_x } => {
-                info!("Player {:?} intends to move with axis_x: {}", event.player, axis_x);
                 let speed = velocity.0.x * axis_x.signum();
                 if speed <= 0.0 {
                     force_limit.x = move_force * axis_x.signum();
@@ -44,7 +39,6 @@ pub(super) fn player_intent_to_force(
                 }
             },
             PlayerIntentKind::JumpStart => {
-                info!("Player {:?} intends to jump", event.player);
                 jump_impulse.y = PLAYER_JUMP_IMPULSE;
             },
             PlayerIntentKind::JumpHold { dt: _ } => {
@@ -54,8 +48,6 @@ pub(super) fn player_intent_to_force(
                 // no end force for now
             },
         }
-        info!("Calculated force limit: {:?}", force_limit);
-        info!("Calculated jump impulse: {:?}", jump_impulse);
         net_force.0 += force_limit;
         impulse.0 += jump_impulse;
     }

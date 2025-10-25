fb72e28bdf2511e3f4cbbd21ff89609c08c5f7a5
Xiaoting Wang <XIW323@pitt.edu>
2025-10-25 11:27:37 +0800

    feat(physics_core): change boundary, origin point now on the left botton corner.

--- src/physics/physics_core/integrate.rs
+++ src/physics/physics_core/integrate.rs
@@ -46,26 +46,26 @@ pub(super) fn velocity_to_transform(
 pub(super) fn boundary(
     mut query: Query<(&mut Transform, &mut Velocity, &mut Momentum)>,
 ) {
-    let width = 1280.0 - 64.0; // minus player width
-    let height = 720.0 - 64.0; // minus player height
+    let width = 1280.0 - 32.0; // minus player width
+    let height = 720.0 - 32.0; // minus player height
     for (mut transform, mut velocity, mut momentum) in query.iter_mut() {
-        if transform.translation.x < - width / 2.0 {
-            transform.translation.x = - width / 2.0;
+        if transform.translation.x < 32.0 {
+            transform.translation.x = 32.0;
             velocity.0.x = 0.0;
             momentum.0.x = 0.0;
         }
-        if transform.translation.x > width / 2.0 {
-            transform.translation.x = width / 2.0;
+        if transform.translation.x > width {
+            transform.translation.x = width;
             velocity.0.x = 0.0;
             momentum.0.x = 0.0;
         }
-        if transform.translation.y < - height / 2.0 {
-            transform.translation.y = - height / 2.0;
+        if transform.translation.y < 32.0 {
+            transform.translation.y = 32.0;
             velocity.0.y = 0.0;
             momentum.0.y = 0.0;
         }
-        if transform.translation.y > height / 2.0 {
-            transform.translation.y = height / 2.0;
+        if transform.translation.y > height {
+            transform.translation.y = height;
             velocity.0.y = 0.0;
             momentum.0.y = 0.0;
         }

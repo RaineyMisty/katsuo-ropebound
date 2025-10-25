b188fbf4653790e54aedddd0a18dcf1fc0696bb6
Xiaoting Wang <XIW323@pitt.edu>
2025-10-25 12:17:27 +0800

    refactor(lifetime): make track seperate

--- src/lifetime/rope_spawn.rs
+++ src/lifetime/rope_spawn.rs
@@ -4,22 +4,13 @@
 // Description: <Player Lifetime - Player Spawn>
 use bevy::prelude::*;
 
-use crate::event::{Player2LifetimeSpawned, Lifetime2RopeSpawn};
+use crate::event::{Lifetime2RopeSpawn};
 use super::component::SpawnTrack;
 
 pub(super) fn wait_for_player_spawn(
     mut track: ResMut<SpawnTrack>,
-    mut events: EventReader<Player2LifetimeSpawned>,
     mut rope_events: EventWriter<Lifetime2RopeSpawn>,
 ) {
-    for event in events.read() {
-        let node = event.node as usize;
-        if track.node_to_entity[node].is_none() {
-            track.node_to_entity[node] = Some(event.entity);
-            track.spawned_players += 1;
-        }
-    }
-
     if !track.is_rope && track.spawned_players == track.expected_players {
         let total_nodes = track.node_to_entity.len();
 
